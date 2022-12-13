dc948205ed1efc7becbe0d7ede684d665659a4ea
MburuJulius <mburujulius1988@gmail.com>
2022-12-13 12:56:13 -0800

    Sizeof data types

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -7,11 +7,11 @@
  */
 int main(void)
 {
-	char=c;
-	int=i;
-	long int=b;
-	long long int=d;
-	float=f;
+	char c;
+	int i;
+	long int b;
+	long long int d;
+	float f;
 
 	printf("size of a char: %lu byte(s)\n", (unsigned long)sizeof(c));
 	printf("size of an int: %lu byte(s)\n", (unsigned long)sizeof(i));
