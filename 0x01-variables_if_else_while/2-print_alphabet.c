e1283c5b6be4a105ec8b5203cf21c02c76758508
MburuJulius <mburujulius1988@gmail.com>
2022-12-16 06:10:54 -0800

    Print lowercase

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - prints alphabetsin lowercase
+ *
+ * return: always 0 (success)
+ */
+
+int main(void)
+
+{
+	char a
+	char z
+
+	putchar('a'..'z')
+
+	putchar(\n)
+
+	retturn (0)
+}
