03d1dbb23d83e5bde1f3d832e1013afef28ce5c4
MburuJulius <mburujulius1988@gmail.com>
2022-12-20 12:10:32 -0800

    Base16

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * main - print base16 numbers
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int n;
+
+	for (n = 48; n < 58; n++)
+	{
+	putchar(n);
+	}
+	for (n = 65; n < 71; n++)
+	{
+	putchar(n);
+	}
+	putchar('\n')
+	return (0);
+}
