ae1a6013b5a955629d32991350425c1d7f48f829
MburuJulius <mburujulius1988@gmail.com>
2022-12-20 12:58:37 -0800

    Combi

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * main - prints single numbers separated by comma and space
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int n;
+
+	n = 48
+	if (n < 58)
+	{
+	putchar(n);
+	while (n != 57)
+	putchar(',');
+	putchar(' ');
+	}
+	putchar('\n');
+	return (0);
+}
