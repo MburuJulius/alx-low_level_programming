7e32f22224aed8395b89f387f36f5b2d726be617
MburuJulius <mburujulius1988@gmail.com>
2022-12-20 11:23:04 -0800

    esrever lowercase alphabet

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - prints lowercase alphabet in reverse using putchar
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char ch;
+
+	for (ch = 'z'; ch >= 'a'; z--)
+	putchar(ch);
+	putchar('\n');
+	return (0);
+}
