91d30ba7e922353ed4896572687e914455642979
MburuJulius <mburujulius1988@gmail.com>
2022-12-20 10:18:00 -0800

    print integers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - prints single digit numbers 
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{	
+	int a = 0;
+
+	while (a < 10)
+	printf("%d\n", a);
+	return (0)
+}
