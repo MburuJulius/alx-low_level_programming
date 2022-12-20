e17d9b8e5822be02075cd3cc158b247ca883a735
MburuJulius <mburujulius1988@gmail.com>
2022-12-20 07:40:13 -0800

    Lower case alphabet

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -10,11 +10,15 @@ int main(void)
 
 {
 	char ch
+	
 	for(ch = 'a'; ch <= 'z'; ch++)
 	{
-		putchar(ch)
-	}
-	putchar(\n)
 	
-	retturn (0)
+	putchar (ch);
+	
+	}
+	
+	putchar (\n);
+	
+	return (0);
 }
