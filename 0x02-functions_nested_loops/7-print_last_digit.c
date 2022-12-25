b4767aeb96783e3bf09c9b3a59ffcada5d841a8c
MburuJulius <mburujulius1988@gmail.com>
2022-12-25 15:02:38 -0800

    Prints last digit

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -2,18 +2,16 @@
 /**
  * print_last_digit - prints the last digit of input number
  *
- * @x: input number
+ * @d: input number
  *
  * Return: returns the last digit
  */
 int print_last_digit(int d)
 {
-	int y = (d % 10);
-
 	if (d < 0)
 	{
 		d = (d * -1);
 	}
-		_putchar(y + '0');
-		return (y);
+		_putchar((d % 10) + '0');
+		return (d % 10);
 }
