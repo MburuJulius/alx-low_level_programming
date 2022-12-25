0e00073ad3c4672a1b10d884b434998d0df3ea00
MburuJulius <mburujulius1988@gmail.com>
2022-12-25 06:32:41 -0800

    Checks if a number is positive or negative

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * print_sign - checks if a number is positive, negative or zero
+ *
+ * @n: the integer to be checked/entered
+ *
+ * Return: returns 1 if n is positive, 0 if n zero, -1if n is negative
+ */
+int print_sign(int n)
+{
+	if (n > 0)
+		_putchar('+');
+	return (1);
+	if (n < 0)
+		_putchar('-');
+	return(-1);
+	else
+		_putchar('0');
+	return (0);
+}
