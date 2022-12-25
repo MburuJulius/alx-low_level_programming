7df62400b852165885445cb08193e14db72a05a7
MburuJulius <mburujulius1988@gmail.com>
2022-12-25 06:13:13 -0800

    This fxn checks for alphabets

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -2,13 +2,13 @@
 /**
  * _isalpha - checks if character entered is an alphabet
  *
- * c: character entered
+ * @c: character entered
  *
  * Return: returns 1 if c is an alphabet and 0 otherwise
  */
 int _isalpha(int c)
 {
-	if ((c >= 'a' || 'A') && (c <= 'z' || 'Z'))
+	if (((c >= 'a') && (c <= 'z')) || ((c >= 'A') && (c <= 'Z')))
 		return (1);
 	else
 		return (0);
