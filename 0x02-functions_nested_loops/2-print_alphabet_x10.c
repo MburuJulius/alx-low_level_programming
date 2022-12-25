61e67f0c1984b56b4b4892e62395fc675ae20096
MburuJulius <mburujulius1988@gmail.com>
2022-12-25 03:48:34 -0800

    Print ten lines of lowercase alphabet

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * print_alphabet_x10 - prints 10 line of lowercase alphabet
+ * followed by a new line
+ *
+ * Return: void
+ */
+void print_alphabet_x10(void)
+{
+	int n;
+	int l;
+
+	{
+		for (n = 'a'; n <= 'z'; n++)
+	{
+		while (l = 0 && l <= 10)
+			_putchar(n);
+	}l++
+
+	}
+	putchar('\n');
+}
