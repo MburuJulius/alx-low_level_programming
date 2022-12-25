8ef7aaf8457a5884780eef23506af2b1200ab74d
MburuJulius <mburujulius1988@gmail.com>
2022-12-25 11:35:30 -0800

    Prints the last digit

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * print_last_digit - prints the last digit of input number
+ *
+ * @x: input number
+ *
+ * Return; returns the last digit
+ */
+int print_last_digit(int x)
+{
+	if ((x < 0) || (x < 0))
+		x = x % 10;
+	{
+		return (x);
+	}
+}
+
