6f7b27386da70811c9380700d9e0f20911fdf175
MburuJulius <mburujulius1988@gmail.com>
2022-12-25 05:33:34 -0800

    Checks for lowercase characters

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * _islower - checks for lowercase characters
+ *
+ * Return: returns 1 for lowercase and 0 otherwise
+ */
+int _islower(int c)
+{
+	int c
+	
+	for (c = 'a'; c <= 'z'; c++)
+		return (1);
+	else 
+		return (0);
+}
