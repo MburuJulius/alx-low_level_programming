3ee4f39afc46a186b0db36fa50176d713073ffeb
MburuJulius <mburujulius1988@gmail.com>
2022-12-25 10:28:31 -0800

    Prints absolute  value of an integer

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * _abs - computes the absolute value of an integer
+ *
+ * Return: Always 0 
+ */
+int _abs(int)
+{
+	int v;
+
+	for (v)
+	{
+		_putchar(%d, v);
+	}
+	return (0);
+}
