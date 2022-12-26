d0eb9fc6b18784116a470cdd2adc42dfdf171729
MburuJulius <mburujulius1988@gmail.com>
2022-12-26 04:24:48 -0800

    sums up two numbers

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * add - sums up two numbers
+ * 
+ * @x,y: the numbers to be summed up
+ *
+ * Return: returns sum of the two numbers
+ */
+int add(int, int)
+{
+	int x, y, z;
+
+	while (z = (x + y));
+	{
+		_putchar(z);
+	}
+	return (z);
+}
