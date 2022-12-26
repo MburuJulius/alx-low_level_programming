a1938c01b912a76ee15335874d08b2550c2c1db9
MburuJulius <mburujulius1988@gmail.com>
2022-12-26 04:59:04 -0800

    Prints numbers 0 to 98

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * print_to_98 - prints all natural numbers from 0 to 98
+ *
+ * Return: void
+ */
+void print_to_98(void)
+{
+	int n;
+
+	for (n = 0, n <= 98, n++)
+	{
+		_putchar(n + '0');
+		_putchar(',');
+		_putchar(' ');
+	}
+	_putchar('\n');
+}
