e6726341c8f9beb6de3be9b145567c56222d043a
MburuJulius <mburujulius1988@gmail.com>
2022-12-29 06:02:31 -0800

    Is 0 positive or negative

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,7 +1,4 @@
-nclude "main.h"
-
-
-
+#include "main.h"
 /**
  *
  * * main - tests function that prints if integer is positive or negative
@@ -9,16 +6,33 @@ nclude "main.h"
  * * Return: 0
  *
  * */
-
-
-
-int main(void)
-
+int main()
 {
 	int i;
 
         i = 0;
- 
 	positive_or_negative(i);
-	return (0);
+	return ();
+}
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ * positive_or_negative - determines if input number is +ve or -ve
+ *
+ * @n: input number
+ *
+ * Return: void
+ */
+void positive_or_negative(int n)
+{
+	srand(time(0));
+
+	n = rand() - RAND_MAX / 2;
+
+	if (n >= 0)
+		printf("%d is positive\n", n);
+	else if (n < 0)
+		printf("%d is negative\n", n);
 }
--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -15,7 +15,7 @@ void times_table(void);
 int add(int, int);
 void print_to_98(int n);
 void print_times_table(int n);
-void positive_or_negative(int i);
+void positive_or_negative(int n);
 
 #endif /* MAIN_H */
 /*Function declarations*/
